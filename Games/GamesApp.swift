55f8cf33285fc0fd628ad80b92b6af23f20c6252
Peter Combee <petercombee@gmail.com>
2024-11-11 19:49:28 +0100

    Add new project

---
+++ Games/GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesApp.swift
+//  Games
+//
+//  Created by Peter Combee on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GamesTests/GamesTests.swift
@@ -0,0 +1,17 @@
+//
+//  GamesTests.swift
+//  GamesTests
+//
+//  Created by Peter Combee on 11/11/2024.
+//
+
+import Testing
+@testable import Games
+
+struct GamesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
