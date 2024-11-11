7a1ba11e3ec6f3fef3c99ccd8e6a3e2d1f4e5448
Peter Combee <petercombee@gmail.com>
2024-11-11 21:42:44 +0100

    `RemoteGamesProvider.getGames` delivers games on valid games JSON data

--- GamesTests/RemoteGamesProviderTests.swift
+++ GamesTests/RemoteGamesProviderTests.swift
@@ -2,19 +2,24 @@
 //  Created by Peter Combee on 11/11/2024.
 //
 
-import Testing
+import XCTest
 
 import Foundation
 
+struct Game: Equatable, Decodable {
+    let id: Int
+    let name: String
+}
+
 final class HttpClientSpy {
     
-    var stub: Error?
+    var stub: Result<Data, Error> = .success(Data())
     
     private(set) var capturedRequest: URLRequest?
     
-    func perform(_ request: URLRequest) throws {
+    func perform(_ request: URLRequest) throws -> Data {
         capturedRequest = request
-        try stub.map { throw $0 }
+        return try stub.get()
     }
 }
 
@@ -24,40 +29,71 @@ struct RemoteGamesProvider {
     init(client: HttpClientSpy) {
         self.client = client
     }
-    
-    func getGames() throws {
+        
+    func getGames() throws -> [Game] {
         let url = URL(string: "https://api.example.com/games")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.httpBody = Data("Fields name;".utf8)
         request.setValue("application/json", forHTTPHeaderField: "Accept")
-        try client.perform(request)
+        let data = try client.perform(request)
+        let games = try JSONDecoder().decode([Game].self, from: data)
+        return games
     }
 }
 
-struct RemoteGamesProviderTests {
-
-    @Test func getGames_performsPostRequest() throws {
+final class RemoteGamesProviderTests: XCTestCase {
+    
+    func test_getGames_performsPostRequest() throws {
         let client = HttpClientSpy()
         let sut = RemoteGamesProvider(client: client)
         
-        try? sut.getGames()
+        _ = try? sut.getGames()
         
-        let request = try #require(client.capturedRequest)
-        
-        #expect(request.httpMethod == "POST")
-        #expect(request.url == URL(string: "https://api.example.com/games")!)
-        #expect(request.value(forHTTPHeaderField: "Accept") == "application/json")
-        #expect(request.httpBody.map { String(data: $0, encoding: .utf8) }  == "Fields name;")
+        let request = try XCTUnwrap(client.capturedRequest)
+    
+        XCTAssertEqual(request.httpMethod, "POST")
+        XCTAssertEqual(request.url, URL(string: "https://api.example.com/games")!)
+        XCTAssertEqual(request.value(forHTTPHeaderField: "Accept"), "application/json")
+        XCTAssertEqual(request.httpBody.map { String(data: $0, encoding: .utf8) }, "Fields name;")
     }
     
-    @Test func getGames_deliversErrorOnClientError() throws {
+    func test_getGames_deliversErrorOnClientError() {
         let client = HttpClientSpy()
         let sut = RemoteGamesProvider(client: client)
-        client.stub = NSError(domain: "any", code: 0)
+        client.stub = .failure(NSError(domain: "any", code: 0))
         
-        #expect(throws: NSError.self) {
-            try sut.getGames()
-        }
+        XCTAssertThrowsError(try sut.getGames())
+    }
+    
+    func test_getGames_deliversGamesOnValidJsonGamesData() {
+        let client = HttpClientSpy()
+        let sut = RemoteGamesProvider(client: client)
+        let gamesJson = """
+        [
+          {
+            "id": 131913,
+            "name": "Maji Kyun! Renaissance"
+          },
+          {
+            "id": 5668,
+            "name": "Commando"
+          },
+          {
+            "id": 95080,
+            "name": "Dotra"
+          }
+        ]
+        """
+        client.stub = .success(Data(gamesJson.utf8))
+        
+        let expectedGames = [
+            Game(id: 131913, name: "Maji Kyun! Renaissance"),
+            Game(id: 5668, name: "Commando"),
+            Game(id: 95080, name: "Dotra")
+        ]
+        
+        let games = try! sut.getGames()
+        XCTAssertEqual(games, expectedGames)
     }
 }
