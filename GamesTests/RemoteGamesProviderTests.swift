b8ed8cd115af6b37e9294efa8b1dafd1083dde38
Peter Combee <petercombee@gmail.com>
2024-11-12 08:56:25 +0100

    Move `RemoteGamesProvider`, `HttpClient`, and `Game` to production

--- GamesTests/RemoteGamesProviderTests.swift
+++ GamesTests/RemoteGamesProviderTests.swift
@@ -3,36 +3,7 @@
 //
 
 import XCTest
-
-import Foundation
-
-struct Game: Equatable, Decodable {
-    let id: Int
-    let name: String
-}
-
-protocol HttpClient {
-    func perform(_ request: URLRequest) throws -> Data
-}
-
-struct RemoteGamesProvider {
-    private let client: HttpClient
-    
-    init(client: HttpClient) {
-        self.client = client
-    }
-        
-    func getGames() throws -> [Game] {
-        let url = URL(string: "https://api.example.com/games")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.httpBody = Data("Fields name;".utf8)
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
-        let data = try client.perform(request)
-        let games = try JSONDecoder().decode([Game].self, from: data)
-        return games
-    }
-}
+@testable import Games
 
 final class RemoteGamesProviderTests: XCTestCase {
     
