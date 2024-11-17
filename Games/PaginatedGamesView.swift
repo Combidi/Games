6e31b379502aee806638ca3517583f1d917f7c7a
Peter Combee <petercombee@gmail.com>
2024-11-17 20:56:01 +0100

    Create PaginatedGamesView.swift

---
+++ Games/PaginatedGamesView.swift
@@ -0,0 +1,80 @@
+//
+//  Created by Peter Combee on 17/11/2024.
+//
+
+import SwiftUI
+
+struct PaginatedGamesView<GameView: View>: View {
+            
+    @ObservedObject private var viewModel: PaginatedGameListViewModel
+    private let makeGameView: (Game) -> GameView
+
+    init(
+        viewModel: PaginatedGameListViewModel,
+        makeGameView: @escaping (Game) -> GameView
+    ) {
+        self.viewModel = viewModel
+        self.makeGameView = makeGameView
+    }
+        
+    var body: some View {
+        Group {
+            switch viewModel.state {
+            case .loading:
+                Text("Loading...")
+                
+            case .loaded(let presentable):
+                listView(for: presentable)
+
+            case .error:
+                Text("Oeps, something went wrong.")
+                
+            }
+        }
+        .task { await viewModel.load() }
+    }
+    
+    private func listView(for games: PresentableGames) -> some View {
+        List {
+            ForEach(games.games, id: \.id) { game in
+                makeGameView(game)
+                    .listRowSeparator(.hidden)
+            }
+            games.loadMore.map(LoadMoreView.init)
+        }
+        .refreshable(action: { await viewModel.reload() })
+    }
+}
+
+struct LoadMoreView: View {
+    
+    let loadMore: () async throws -> Void
+    
+    private enum LoadMoreState {
+        case loading
+        case error
+    }
+    
+    @State private var state: LoadMoreState = .loading
+    
+    var body: some View {
+        switch state {
+        case .loading:
+            Text("Loading more...")
+                .onAppear { load() }
+        case .error:
+            Button(action: load) {
+                Text("Retry")
+            }
+        }
+    }
+    
+    private func load() {
+        Task {
+            do { try await loadMore() }
+            catch {
+                state = .error
+            }
+        }
+    }
+}
