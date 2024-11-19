31d6fc6906e7cf94ba8a7ad2cba6fd9258a2f0e1
Peter Combee <petercombee@gmail.com>
2024-11-19 10:06:52 +0100

    move `RemotePaginatedGamesProvider` to its own file in production

---
+++ Games/RemotePaginatedGamesProvider.swift
@@ -0,0 +1,42 @@
+//
+//  Created by Peter Combee on 19/11/2024.
+//
+
+protocol RemoteGamesProvider {
+    func getGames(limit: Int, offset: Int) async throws -> [Game]
+}
+
+struct RemotePaginatedGamesProvider {
+        
+    private let startOffset: Int
+    private let remoteGamesProvider: RemoteGamesProvider
+    
+    init(
+        startOffset: Int = 0,
+        remoteGamesProvider: RemoteGamesProvider
+    ) {
+        self.startOffset = startOffset
+        self.remoteGamesProvider = remoteGamesProvider
+    }
+    
+    func getGames() async throws -> PaginatedGames {
+        let games = try await remoteGamesProvider.getGames(limit: 10, offset: startOffset)
+        return PaginatedGames(games: games, loadMore: makeRemoteLoadMoreLoader(currentGames: games))
+    }
+    
+    private func makeRemoteLoadMoreLoader(currentGames: [Game]) -> () async throws -> PaginatedGames {
+        return {
+            let nextBatchOfGames = try await remoteGamesProvider.getGames(
+                limit: 10,
+                offset: currentGames.count + startOffset
+            )
+            let reachedEnd = nextBatchOfGames.count != 10
+            let games = currentGames + nextBatchOfGames
+            let page =  PaginatedGames(
+                games: games,
+                loadMore: reachedEnd ? nil : makeRemoteLoadMoreLoader(currentGames: games)
+            )
+            return page
+        }
+    }
+}
--- GamesTests/RemotePaginatedGamesProviderTests.swift
+++ GamesTests/RemotePaginatedGamesProviderTests.swift
@@ -5,45 +5,6 @@
 import XCTest
 @testable import Games
 
-private protocol RemoteGamesProvider {
-    func getGames(limit: Int, offset: Int) async throws -> [Game]
-}
-
-private struct RemotePaginatedGamesProvider {
-        
-    private let startOffset: Int
-    private let remoteGamesProvider: RemoteGamesProvider
-    
-    init(
-        startOffset: Int = 0,
-        remoteGamesProvider: RemoteGamesProvider
-    ) {
-        self.startOffset = startOffset
-        self.remoteGamesProvider = remoteGamesProvider
-    }
-    
-    func getGames() async throws -> PaginatedGames {
-        let games = try await remoteGamesProvider.getGames(limit: 10, offset: startOffset)
-        return PaginatedGames(games: games, loadMore: makeRemoteLoadMoreLoader(currentGames: games))
-    }
-    
-    private func makeRemoteLoadMoreLoader(currentGames: [Game]) -> () async throws -> PaginatedGames {
-        return {
-            let nextBatchOfGames = try await remoteGamesProvider.getGames(
-                limit: 10,
-                offset: currentGames.count + startOffset
-            )
-            let reachedEnd = nextBatchOfGames.count != 10
-            let games = currentGames + nextBatchOfGames
-            let page =  PaginatedGames(
-                games: games,
-                loadMore: reachedEnd ? nil : makeRemoteLoadMoreLoader(currentGames: games)
-            )
-            return page
-        }
-    }
-}
-
 final class RemotePaginatedGamesProviderTests: XCTestCase {
     
     func test_getGames_loadsFirstPageFromRemoteLoader() async throws {
