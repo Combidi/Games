02338b1b465d93e2db7c74265b94138a151dcce4
Peter Combee <petercombee@gmail.com>
2024-11-19 20:49:21 +0100

    `CachingGamesProviderDecorator.getGames` delivers gamed received from provider

---
+++ GamesTests/CachingGamesProviderDecodatorTests.swift
@@ -0,0 +1,46 @@
+//
+//  Created by Peter Combee on 19/11/2024.
+//
+
+import XCTest
+@testable import Games
+
+private struct CachingGamesProviderDecorator {
+    
+    private let provider: PaginatedGamesProviderStub
+    
+    init(provider: PaginatedGamesProviderStub) {
+        self.provider = provider
+    }
+    
+    func getGames() throws -> PaginatedGames {
+        try provider.getGames()
+    }
+}
+
+final class CachingGamesProviderDecodatorTests: XCTestCase {
+    
+    func test_getGames_deliversGamesReceivedFromProvider() throws {
+        let provider = PaginatedGamesProviderStub()
+        let sut = CachingGamesProviderDecorator(provider: provider)
+        let games = [
+            Game(id: 0, name: "first", imageId: nil),
+            Game(id: 1, name: "second", imageId: nil)
+        ]
+        provider.stub = .success(PaginatedGames(games: games, loadMore: nil))
+        
+        let result = try sut.getGames()
+        
+        XCTAssertEqual(result.games, games)
+    }
+}
+
+// MARK: - Helpers
+
+private final class PaginatedGamesProviderStub: PaginatedGamesProvider {
+    var stub: Result<PaginatedGames, Error> = .success(PaginatedGames(games: [], loadMore: nil))
+    
+    func getGames() throws -> PaginatedGames {
+        try stub.get()
+    }
+}
