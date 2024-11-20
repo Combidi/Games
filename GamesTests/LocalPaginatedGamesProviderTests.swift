d552ee2390ba2716b2108d40ac815c1f9a1e7a21
Peter Combee <petercombee@gmail.com>
2024-11-20 15:19:41 +0100

    `LocalPaginatedGamesProvider.loadMore` loads more using given loadMore handler

--- GamesTests/LocalPaginatedGamesProviderTests.swift
+++ GamesTests/LocalPaginatedGamesProviderTests.swift
@@ -14,14 +14,18 @@ private struct LocalPaginatedGamesProvider {
     struct MissingGamesError: Error {}
     
     private let cache: GameCacheRetrievable
+    private let loadMore: (Int) -> PaginatedGames
     
-    init(cache: GameCacheRetrievable) {
+    init(cache: GameCacheRetrievable, loadMore: @escaping (Int) -> PaginatedGames) {
         self.cache = cache
+        self.loadMore = loadMore
     }
     
     func getGames() throws -> PaginatedGames {
         guard let games = cache.cachedGames() else { throw MissingGamesError() }
-        return PaginatedGames(games: games, loadMore: nil)
+        return PaginatedGames(games: games, loadMore: {
+            loadMore(games.count)
+        })
     }
 }
 
@@ -33,23 +37,55 @@ final class LocalPaginatedGamesProviderTests: XCTestCase {
             Game(id: 1, name: "second", imageId: nil)
         ]
         let cache = Cache(games: games)
-        let sut = LocalPaginatedGamesProvider(cache: cache)
-        
+        let sut = LocalPaginatedGamesProvider(
+            cache: cache,
+            loadMore: { _ in PaginatedGames(games: [], loadMore: nil) }
+        )
+
         let result = try sut.getGames()
         
         XCTAssertEqual(result.games, games)
     }
-
+    
     func test_getGames_withoutCachedGames_deliversError() {
         let cache = Cache(games: nil)
-        let sut = LocalPaginatedGamesProvider(cache: cache)
-                
+        let sut = LocalPaginatedGamesProvider(
+            cache: cache,
+            loadMore: { _ in PaginatedGames(games: [], loadMore: nil) }
+        )
+
         do {
             let result = try sut.getGames()
             XCTFail("Expected getGames to throw error, got \(result) instead")
         } catch {
             XCTAssertTrue(error is LocalPaginatedGamesProvider.MissingGamesError)
         }
+    }
+
+    func test_loadMore_loadMoreProvidingOffsetFromWhichToLoadMore() async throws {
+        let amountOfCachedGames = 12
+        let cachedGames = Array(
+            repeating: Game(id: 0, name: "any", imageId: nil),
+            count: amountOfCachedGames
+        )
+        let cache = Cache(games: cachedGames)
+        var capturedOffset: Int?
+        let additionalGames = [
+            Game(id: 1, name: "additionalGame", imageId: nil)
+        ]
+        let sut = LocalPaginatedGamesProvider(
+            cache: cache,
+            loadMore: { offset in
+                capturedOffset = offset
+                return PaginatedGames(games: cachedGames + additionalGames, loadMore: nil)
+            }
+        )
+        let firstPage = try sut.getGames()
+        
+        let secondPage = try await firstPage.loadMore?()
+        
+        XCTAssertEqual(capturedOffset, amountOfCachedGames)
+        XCTAssertEqual(secondPage?.games, cachedGames + additionalGames)
     }
 }
 
